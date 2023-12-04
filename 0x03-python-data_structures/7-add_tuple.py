9dc09b4cfc876a2c66178ff3202aeddb15604a3f
hanaaattya <hanaaattya6565@gmail.com>
2023-12-05 00:04:13 +0300

    m

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b1 = tuple_b[0] if len(tuple_b) > 0 else 0
     b2 = tuple_b[1] if len(tuple_b) > 1 else 0
 
-    total = (a[0] + b[0], a[1] + b[1])
+    total = (a1 + b1, a2 + b2)
 
     return total
