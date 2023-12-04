037d0e295552ed88fbe8abdd84b1ec7c8616351a
hanaaattya <hanaaattya6565@gmail.com>
2023-12-05 00:24:03 +0300

    m

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None
+    return None
+    cpy_list = []
+    for num in my_list:
+        if (num % 2) == 0:
+            cpy_list.append(True)
+        else:
+            cpy_list.append(False)
+    return cpy_list
