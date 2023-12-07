eb4d43f624683165224348226f3519161ac387e4
hanaaattya <hanaaattya6565@gmail.com>
2023-12-07 05:30:02 +0300

    m

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [replace if item == search else item for item in my_list]
+    new = [replace if element == search else element for element in my_list]
     return new
